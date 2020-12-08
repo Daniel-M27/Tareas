e309dacd6dd5cc3c195163bae2ca37ea070db658
Daniel-M27 <daniel.ll27@ciencias.unam.mx>
2020-12-08 16:04:44 -0700

    Problema

---
+++ Python_Scripts/base.py
@@ -0,0 +1,71 @@
+print("""
+Este programa es un convertidor de enteros a diferentes bases numéricas. La versión actual soporta solamente las bases del 2 al 10.
+
+Para convertir un entero en Base-n a decimal, oprima [1].
+Para convertir un entero decimal a Base-n, oprima [2].
+""")
+
+mode = input('> ')
+if mode == '1':
+    base = int(input('Base: '))
+
+    if base > 10:
+        print("Base no soportada.")
+        exit()
+
+    bin = list(input('Número: '))
+    order = int(len(bin)) - 1
+    filter = all(int(x) < base for x in bin)
+
+    num = []
+    ind = []
+    pow = []
+    times = []
+
+    if filter == True:
+        for i, x in enumerate(bin):
+            if x != '0':
+                ind.append(i)
+
+        for x in ind:
+            pow.append(base**(order - x))
+
+        for x in bin:
+            if x != '0':
+                num.append(int(x))
+
+        for j in range(len(num)):
+            times.append(num[j] * pow[j])
+
+        print(sum(times))
+
+    else:
+        print(f"Error, valor inválido en Base-{base}.")
+
+elif mode == '2':
+    from math import floor
+    from decimal import Decimal
+
+    num = int(input('Número: '))
+    base = int(input('Base: '))
+
+    if base > 10:
+        print("Base no soportada.")
+        exit()
+
+    mod = []
+
+    def reverse(list):                               # Invierte el orden de elementos en una lista.
+        return [x for x in reversed(list)]
+
+    def cat(list):                                   # Concatena los elementos de una lista en un string.
+        blank = ''
+        return blank.join(list)
+
+    while base <= num:
+        mod.append(str(num % base))
+        num = floor(Decimal(num) / Decimal(base))
+    mod.append(str(num))
+
+    convert = reverse(mod)
+    print(cat(convert))
